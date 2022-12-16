3798d038d0b662b64ee839303142e8e4bd056bca
Matthias <matthias.karl@cmdscale.com>
2022-12-16 13:45:54 +0100

    feat: setup netrw sidebar

--- lua/matthias/set.lua
+++ lua/matthias/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2 
+vim.opt.softtabstop = 2 
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -15,3 +15,10 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.g.mapleader = " "
+
+-- netrw
+vim.g.netrw_liststyle = 3 -- tree view
+vim.g.netrw_banner = 0 -- disable banner
+vim.g.netrw_browse_split = 2 -- vertical split
+vim.g.netrw_winsize = 80 
+
